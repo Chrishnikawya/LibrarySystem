237836166b91e280fd7d9673c1a7c147e45f55df
Chrishnikawya <chrishnikawya31@gmail.com>
2024-07-24 14:34:30 +0530

    controlllers

---
+++ LibrarySystemBackend/LibrarySystem/LibrarySystem/Controllers/ResevationController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LibrarySystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResevationController : ControllerBase
+    {
+        // GET: api/<ResevationController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ResevationController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ResevationController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ResevationController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ResevationController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

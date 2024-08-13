2f9081606f4e606e61549a7e9bcbbfa76328378e
Chrishnikawya <chrishnikawya31@gmail.com>
2024-08-13 13:39:53 +0530

    Changes

---
+++ LibrarySystemFrontEnd/library-management-system/src/views/BookView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="book">
+    <h1>Books</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BookView'
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ LibrarySystemFrontEnd/library-management-system/src/views/ReservationView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="reservation">
+    <h1>Reservations</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ReservationView'
+};
+</script>
+
+<style scoped>
+
+</style>
